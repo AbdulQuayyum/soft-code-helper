4ee9c29024cf28fae61b64ae1b2254b26beaea84
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-01 02:22:39 +0100

    Chief Cooker is cooking 👨🏾‍🍳

--- Frontend/Assets/Javascript/Main.js
+++ Frontend/Assets/Javascript/Main.js
@@ -1,10 +1,39 @@
 import bot from "../Images/bot.svg"
 import user from "../Images/user.svg"
 
-const from = document.querySelector("form")
-const chatContainer = document.querySelector("#ChatContainer")
+const Form = document.querySelector("form")
+const ChatContainer = document.querySelector("#ChatContainer")
 
 let LoadInterval
+
+function ChatStripe(isAI, Value, UniqueID) {
+    return (
+        `
+        <div class="wrapper ${isAI && 'AI'}">
+        <div class="chat">
+            <div class="profile">
+                <img 
+                  src=${isAI ? bot : user} 
+                  alt="${isAI ? 'bot' : 'User'}" 
+                />
+            </div>
+            <div class="message" id=${UniqueID}>${Value}</div>
+        </div>
+    </div>
+        `
+    )
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function GenerateUniqueID() {
+    const Timestamp = Date.now()
+    const RandomNumber = Math.random()
+    const HexadecimalString = RandomNumber.toString(16)
+
+    return `id-${Timestamp}-${HexadecimalString}`
+}
 
 function Loader(element) {
     element.textContent = ""
@@ -31,31 +60,33 @@ function TypeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
-function GenerateUniqueId() {
-    const Timestamp = Date.now()
-    const RandomNumber = Math.random()
-    const HexadecimalString = RandomNumber.toString(16)
+const HandleSubmit = async (e) => {
+    e.preventDefault()
 
-    return `id-${Timestamp}-${HexadecimalString}`
+    const data = new FormData(Form)
+
+    // User's Chatstripe
+    ChatContainer.innerHTML += ChatStripe(false, data.get("prompt"))
+
+    // To clear the textarea input
+    Form.reset()
+
+    // Bot's Chatstripe
+    const UniqueID = GenerateUniqueID()
+    ChatContainer.innerHTML += ChatStripe(true, " ", UniqueID)
+
+    // To focus scroll to the bottom
+    ChatContainer.scrollTop = ChatContainer.scrollHeight
+
+    // Specific Message Div
+    const MessageDiv = document.getElementById(UniqueID)
+
+    Loader(MessageDiv)
 }
 
-function ChatStripe(isAI, Value, UniqueID) {
-    return (
-        `
-        <div class="wrapper ${isAI && 'AI'}">
-        <div class="chat">
-            <div class="profile">
-                <img 
-                  src=${isAI ? bot : user} 
-                  alt="${isAI ? 'bot' : 'User'}" 
-                />
-            </div>
-            <div class="message" id=${UniqueID}>${Value}</div>
-        </div>
-    </div>
-        `
-    )
-}
+Form.addEventListener("submit", HandleSubmit)
+Form.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        HandleSubmit(e)
+    }
+})
